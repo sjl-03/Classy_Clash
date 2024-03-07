e50dd62b057b1a7d017ba980aaac54ceeb6992e2
sjl-03 <shenjunlu03@gmail.com>
2024-03-07 22:09:30 +0000

    Added map

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    const int WINDOW_WDITH = 384;
+    const int WINDOW_HEIGHT = 384;
+    
+    InitWindow(WINDOW_WDITH, WINDOW_HEIGHT, "Top down");
 
+    Texture2D map = LoadTexture("nature_tileset/mymap.png");
+    float mapX {};
+
+    SetTargetFPS(64);
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            Vector2 mapPos {mapX, 0.0};
+            DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
         EndDrawing();
     }
-
+    UnloadTexture(map);
     CloseWindow();
 
     return 0;
---
+++ nature_tileset/RPG Nature Tileset Autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RPG Nature Tileset Autumn" tilewidth="32" tileheight="32" tilecount="135" columns="15">
+ <image source="RPG Nature Tileset Autumn.png" trans="000000" width="480" height="288"/>
+</tileset>
